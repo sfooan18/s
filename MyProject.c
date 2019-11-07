8bf3d3c039caa4646c0ab5accbdfd8680048e384
sfooan18 <55088510+sfooan18@users.noreply.github.com>
2019-11-07 08:17:09 +0300

    المرحلة الثالثة ( الكود صحيح -- 2 سويتش

--- MyProject.c
+++ MyProject.c
@@ -1,10 +1,17 @@
 void main() {
 
- char countrer=0;
+  trisb.RA0 = 1; //RA0 AS INBUT
+  
+  TRISb.RA1 = 0; PORTB.RA1= 0; // RA1 AS OUTBUT
+  
 
+   loop:
+  
+     if (PORTB.RA0 == 0) // IF SWITCH HAS  PRSSED
+     {
+        portb.RA1 = 1;   //tern on led
+      }
 
-
- goto loop;
- 
-
+  goto loop;
+  
 }
