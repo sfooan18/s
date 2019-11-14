7a48372dd378a7a600e16cc626b66aaac660aad9
sfooan18 <55088510+sfooan18@users.noreply.github.com>
2019-11-14 08:39:30 +0300

    المرحلة قبل الاخيرة ( تشغيل ليدات

--- MyProject12.c
+++ MyProject12.c
@@ -1,9 +1,11 @@
+void main() {
 
+}
+Display one fixed character : A
+Rows scanning = PORTB connected to anodes ,
+Data connected to PORTC , cathodes
 
-
-
-
-
+*******************************************************/
 //Define Matrix Character Information as 8 byte array
 unsigned char font[8]={0x3E,0x7e,0xC8,0xC8,0x7E,0x3E,0x00,0x00}; // A
 unsigned char row_select[8]={1,2,4,8,16,32,64,128};
@@ -15,13 +17,13 @@ TRISC=0;
 PORTB=0;
 PORTC=0;
 
-
+  while(1)
   {
    for(i=0 ; i<8 ; I++)
    {
    PORTB=row_select[i]; //select row as array element
    PORTC=~font[i];//send data to port C as array element
-
+   delay_ms(5); // short delay
   }
       }
 }
